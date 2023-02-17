426373e9186e12078dae2e2c102f6e0e975d8329
kdk-creator <kelvindkelvin2019@gmail.com>
2023-02-16 18:16:52 -0800

    4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
-
 /**
- * main - prints "Programming is like building a multilingual
+ * main - Entry
  * puzzle,followed by a newline"
  * return: Always  0
  */
-
-
 int main(void)
-
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
