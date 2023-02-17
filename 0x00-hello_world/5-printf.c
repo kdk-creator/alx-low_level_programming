368a377f4204b89266325bf58e0bc28732c261bd
kdk-creator <kelvindkelvin2019@gmail.com>
2023-02-16 19:25:28 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
-
 /**
- * main -"with proper grammer, but the outcome
- * is a piece of art," followed newline 
- * Return: Always 0.
+ * main - Entry
+ * Return: Always 0
  */
-
 int main(void)
-
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
