f623503a812e80064eaab4db0c62831f2047dc78
kdk-creator <kelvindkelvin2019@gmail.com>
2023-02-17 23:19:01 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a' le--)
+		putchar(le);
+	putchar('\n');
+	return (0);
+}
