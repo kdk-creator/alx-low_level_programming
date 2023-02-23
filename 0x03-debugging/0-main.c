b450a10cceb124644dc22a03b60136f5ec656241
kdk-creator <kelvindkelvin2019@gmail.com>
2023-02-22 22:53:53 -0800

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.c"
+
 /**
  * main - test functions that print if a number is positive or not
  * Return: 0
  */
+
 int main(void)
 
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
