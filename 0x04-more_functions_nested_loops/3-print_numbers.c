528a83dd14590ca8354d48c93742175214a2556e
kdk-creator <kelvindkelvin2019@gmail.com>
2023-02-23 17:39:45 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - print 0-9
- * return: void
+ * return: The number since 0 up to 9
  */
 
 void print_numbers(void)
 {
 	int x;
 
-	for (x = '0'; x <= '9'; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(x + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
