452e568abbae5593726a151d431eed2c11fc1cfc
kdk-creator <kelvindkelvin2019@gmail.com>
2023-02-27 07:45:06 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as paramter and update to 98
+ * @n: number to be updated
+ * Return: nothing on success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
