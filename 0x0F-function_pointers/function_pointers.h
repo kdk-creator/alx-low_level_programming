dfb6b5f37acf6affce376bfacb46de698d95fba9
kdk-creator <kelvindkelvin2019@gmail.com>
2023-03-23 03:01:26 -0700

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 #include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
