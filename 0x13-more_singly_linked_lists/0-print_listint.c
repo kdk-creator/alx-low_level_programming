5b7f2d39ed9755531ce2b6ad4c9c8459f05c746c
kdk-creator <kelvindkelvin2019@gmail.com>
2023-04-03 09:29:13 -0700

    task 0. Print list completed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,9 +8,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
-	while(h)
-		printf("%d\n", h ->n);
+	while (h)
+		printf("%d\n", h->n);
 	num++;
-	h = h ->next;
+	h = h->next;
 	return (num);
 }
