898c8d326eac47bcc31a708dc5f15af473e2bcdd
kdk-creator <kelvindkelvin2019@gmail.com>
2023-04-04 08:39:31 -0700

    task 3 add node int end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,9 +14,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
